224817bb26b6bbac5a7bb28c5b4eeae33936b7f4
Alfred A <20140832+carrotsnotfound@users.noreply.github.com>
2021-05-28 13:33:29 +0800

    more ENV woes

--- nonDockerInstall.sh
+++ nonDockerInstall.sh
@@ -4,11 +4,17 @@ DA_ROOTFOLDER=""
 CLONE_FOLDER=""
 CIAO_INSTALL="./ciao/build/bin"
 
+activate () {
+  . /usr/share/docassemble/local3.8/bin/activate 
+}
+
 # installing ciao & scasp
 git clone https://github.com/ciao-lang/ciao
 cd ciao; ./ciao-boot.sh local-install
 
-export PATH=$CIAO_INSTALL;$PATH
+if [ -x /tmp/l4-docassemble/ciao/build/bin/ciao-env ] ; then
+  eval "$(/tmp/l4-docassemble/ciao/build/bin/ciao-env --sh)"
+fi
 ciao get gitlab.software.imdea.org/ciao-lang/sCASP
 
 # cloning the required docassemble extensions 
@@ -17,7 +23,7 @@ git clone https://github.com/smucclaw/docassemble-scasp
 git clone https://github.com/smucclaw/docassemble-l4
 git clone https://github.com/smucclaw/gf-python
 
-/usr/share/docassemble/local3.8/bin/activate # TODO: not the best way of handling this
+activate
 pip3 install --upgrade \
   ./docassemble-datatypes \
   ./docassemble-scasp \
