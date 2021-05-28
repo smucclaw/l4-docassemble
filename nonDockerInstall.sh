6bd60efaa964d3313ff6da6fec676fdee301eb89
Alfred A <20140832+carrotsnotfound@users.noreply.github.com>
2021-05-28 13:20:02 +0800

    remove su line, to add in README

--- nonDockerInstall.sh
+++ nonDockerInstall.sh
@@ -10,14 +10,12 @@ cd ciao; ./ciao-boot.sh local-install
 source ~/.bashrc # TODO: generalize this for sh-compatible shells
 ciao get gitlab.software.imdea.org/ciao-lang/sCASP
 
-
 # cloning the required docassemble extensions 
 git clone https://github.com/smucclaw/docassemble-datatypes 
 git clone https://github.com/smucclaw/docassemble-scasp
 git clone https://github.com/smucclaw/docassemble-l4
 git clone https://github.com/smucclaw/gf-python
 
-su www-data
 source /usr/share/docassemble/local3.8/bin/activate # TODO: not the best way of handling this
 pip3 install --upgrade \
   ./docassemble-datatypes \
