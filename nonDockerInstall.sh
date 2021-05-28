e84f7ede94e311f81586d2c1c8ec8a17180dc947
Alfred A <20140832+carrotsnotfound@users.noreply.github.com>
2021-05-28 13:01:48 +0800

    Adding sCASP installation instructions

--- nonDockerInstall.sh
+++ nonDockerInstall.sh
@@ -7,6 +7,9 @@ CLONE_FOLDER=""
 git clone https://github.com/ciao-lang/ciao
 cd ciao; ./ciao-boot.sh local-install
 
+source ~/.bashrc # TODO: generalize this for sh-compatible shells
+ciao get gitlab.software.imdea.org/ciao-lang/sCASP
+
 
 # cloning the required docassemble extensions 
 git clone https://github.com/smucclaw/docassemble-datatypes 
@@ -14,6 +17,8 @@ git clone https://github.com/smucclaw/docassemble-scasp
 git clone https://github.com/smucclaw/docassemble-l4
 git clone https://github.com/smucclaw/gf-python
 
+su www-data
+source /usr/share/docassemble/local3.8/bin/activate # TODO: not the best way of handling this
 pip3 install --upgrade \
   ./docassemble-datatypes \
   ./docassemble-scasp \
