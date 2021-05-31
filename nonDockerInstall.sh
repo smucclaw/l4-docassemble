18647bef43a646c15da758eefbd8ba0dc46eae35
Alfred A <20140832+carrotsnotfound@users.noreply.github.com>
2021-05-31 10:05:58 +0800

    I wonder if Requirement already satisfied: pgf in /usr/local/lib/python3.9/site-packages (1.0) works

--- nonDockerInstall.sh
+++ nonDockerInstall.sh
@@ -4,7 +4,7 @@ activate () {
   . /usr/share/docassemble/local3.8/bin/activate 
 }
 
-# installing ciao & scasp
+echo "[1]: Installing ciao & sCASP\n"
 git clone https://github.com/ciao-lang/ciao
 cd ciao; ./ciao-boot.sh local-install
 if [ -x ./ciao/build/bin/ciao-env ] ; then
@@ -12,19 +12,26 @@ if [ -x ./ciao/build/bin/ciao-env ] ; then
 fi
 ciao get gitlab.software.imdea.org/ciao-lang/sCASP
 
-# cloning the required docassemble extensions 
+echo "[2]: Cloning the required docassemble extensions\n"
 git clone https://github.com/smucclaw/docassemble-datatypes 
 git clone https://github.com/smucclaw/docassemble-scasp
 git clone https://github.com/smucclaw/docassemble-l4
 git clone https://github.com/smucclaw/gf-python
 
+echo "[3]: Installing the required docassemble extensions\n"
 activate
 pip3 install --upgrade \
   ./docassemble-datatypes \
   ./docassemble-scasp \
-  ./docassemble-l4
-# refresh docassemble
+  ./docassemble-l4 \
+  pgf
+
+echo "[4]: Refreshing docassemble"
 python -m docassemble.webapp.create_tables
 
-## TODO: Add instructions about apiKeys
+# TODO 
+# echo "[5]: Moving rps.pl & rps.yml to playground for use in testing"
+
+
+## TODO: Add instructions about apiKeys 
 echo "\n\nInstallation complete: \nPlease follow the instructions within README.md on API-keys, before going through the tests & ensuring your installation runs smoothly.\n"
